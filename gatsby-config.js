fa4e0e4676990ad2f15f4485d1fb81bf806b7017
GeisonRDS <geisonrobertodasilva@gmail.com>
2020-06-25 12:59:48 -0300

    Initial project commit.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Blog`,
+    description: `A blog about web development, API and mobile, linux settings and tricks and other cool tech stuff.`,
+    author: `@GeisonRDS`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
